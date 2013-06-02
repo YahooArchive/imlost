d900f1cf89b2bed778cccc99dba9ed9bcb1bb292
Chang Luo <changluo@yahoo-inc.com>
2013-06-02 16:57:10 -0700

    Added afnetowrking cocoapod

---
+++ imlost/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFHTTPRequestOperationLogger
+#define COCOAPODS_POD_AVAILABLE_AFHTTPRequestOperationLogger
+#define COCOAPODS_VERSION_MAJOR_AFHTTPRequestOperationLogger 0
+#define COCOAPODS_VERSION_MINOR_AFHTTPRequestOperationLogger 10
+#define COCOAPODS_VERSION_PATCH_AFHTTPRequestOperationLogger 0
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 2
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
